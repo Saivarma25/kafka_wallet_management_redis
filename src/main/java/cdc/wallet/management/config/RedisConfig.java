2ad59982eabad4fd836ca1e7ab2611b2dd72767a
Saivarma25 <saivarma.akarapu@vannainfotech.com>
2025-04-10 18:32:04 +0530

    Changes to RedisConfig.java

--- src/main/java/cdc/wallet/management/config/RedisConfig.java
+++ src/main/java/cdc/wallet/management/config/RedisConfig.java
@@ -13,8 +13,8 @@ import java.math.BigDecimal;
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, BigDecimal> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, BigDecimal> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericToStringSerializer<>(BigDecimal.class));
